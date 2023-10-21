348c7f89f423ceedb9ee7c7cfe6ac9da87ddac33
AvdeepKaur <123137451+AvdeepKaur@users.noreply.github.com>
2023-10-20 21:26:07 -0400

    the base js code

---
+++ scripts.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+        
+    function handleCancelClick(buttonName) {
+        var post = document.getElementById("postPopup");
+        post.style.display = "block";
+    }
+
+    function handlePostClick(buttonName) {  
+        var postUser = document.getElementById("User").value;
+        var postContent = document.getElementById("content").value;
+
+        var postElement = document.createElement("div");
+        postElement.className = "post";
+        postElement.innerHTML = "<h3>" + postTitle + "</h3><p>" + postContent + "</p>";
+
+        document.getElementById("posts").appendChild(postElement);
+
+        closePopup();
+    }
+    
+
+    var cancelButton = this.getElementById("id of the button");
+    cancelButton.addEventListener('click', function(event) {handleCancelClick("cancelButton")});
+
+    var postButton = this.getElementById("insert id of the post button");
+    postButton.addEventListener('click', function(event) {handlePostClick(buttonName)});
+
+
+
+});
+
+
+
