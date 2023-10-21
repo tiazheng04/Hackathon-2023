f63a01af244a921f9b043ae5a2bde490b0ce22f8
AvdeepKaur <123137451+AvdeepKaur@users.noreply.github.com>
2023-10-21 12:06:00 -0400

    names

--- scripts.js
+++ scripts.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-        
-    function handleCancelClick(buttonName) {
-        var post = document.getElementById("postPopup");
-        post.style.display = "block";
-    }
+  //handler for cancelling a post
+  function handleCancelClick(buttonName) {
+    var post = document.getElementById("postPopup");
+    post.style.display = "block";
+  }
 
-    function handlePostClick(buttonName) {
-      var postUser = document.getElementById("User").value;
-      var title = doctument.getElementById("title").value;
-      var postContent = document.getElementById("content").value;
+  //handler for the post click
+  function handlePostClick(buttonName) {
+    var postUser = document.getElementById("User").value;
+    var title = doctument.getElementById("title").value;
+    var postContent = document.getElementById("content").value;
 
-      var postElement = document.createElement("div");
-      postElement.className = "posts-box";
-      postElement.innerHTML =
-        "<h3>" + postTitle + "</h3><p>" + postContent + "</p>";
+    var postElement = document.createElement("div");
+    postElement.className = "posts-box";
+    postElement.innerHTML =
+      "<h3>" + postTitle + "</h3><p>" + postContent + "</p>";
 
-      document.getElementById("posts").appendChild(postElement);
+    document.getElementById("posts").appendChild(postElement);
 
-      closePopup();
-    }
+    closePopup();
+  }
 
+  //adds listeners to the buttons
+  var cancelButton = this.getElementById("cancel-btn");
+  cancelButton.addEventListener("click", function (event) {
+    handleCancelClick(cancelButton);
+  });
 
-    var cancelButton = this.getElementById("cancel");
-    cancelButton.addEventListener("click", function (event) {
-      handleCancelClick("cancelButton");
-    });
-
-    var postButton = this.getElementById("post");
-    postButton.addEventListener('click', function(event) {handlePostClick(buttonName)});
-
-
-
+  var postButton = this.getElementById("post-btn");
+  postButton.addEventListener("click", function (event) {
+    handlePostClick(postButton);
+  });
 });
 
 
