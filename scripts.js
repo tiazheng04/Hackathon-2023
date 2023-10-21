517983c4927fd8500e77a4ecded0e2fa5054bd36
AvdeepKaur <123137451+AvdeepKaur@users.noreply.github.com>
2023-10-21 13:50:11 -0400

    added prototype search function and doesn't post if values are empty

--- scripts.js
+++ scripts.js
@@ -1,24 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //connects this js file to the firebase database
+  firebaseOperation();
+
   //handler for cancelling a post by clearing the previous text
   function handleCancelClick(buttonName) {
-    document.getElementById("title").value = " ";
-    document.getElementById("content").value = " ";
+    document.getElementById("title").value = "";
+    document.getElementById("content").value = "";
     document.getElementById("draft-box").style.zIndex = "-1";
     document.getElementById("posts-box").style.filter = "blur(0px)";
   }
 
   //handler for the post click
+  //if empty don't post and poses an alert
   function handlePostClick(buttonName) {
-    let postTitle = document.getElementById("title").value;
-    let postContent = document.getElementById("content").value;
+    let postTitle = document.getElementById("title").value.trim();
+    let postContent = document.getElementById("content").value.trim();
     let oldContent = document.getElementById("posts-box").innerHTML;
-    document.getElementById("posts-box").innerHTML = `
-      <div class="post">
-          <h2 id="post-title">${postTitle}</h2>
-          <p>${postContent}</p>
-      </div>
-      ${oldContent}
-  `;
+    if (postTitle != " " && postContent != " ") {
+      document.getElementById("posts-box").innerHTML = `
+          <div class="post">
+              <h2 id="post-title">${postTitle}</h2>
+              <p>${postContent}</p>
+          </div>
+          ${oldContent}
+      `;
+    } else {
+      alert("Please write in both text boxes");
+    }
     document.getElementById("draft-box").style.zIndex = "-1";
     document.getElementById("posts-box").style.filter = "blur(0px)";
   }
@@ -26,6 +34,28 @@ document.addEventListener("DOMContentLoaded", function () {
   function handleDraftClick() {
     document.getElementById("draft-box").style.zIndex = "1";
     document.getElementById("posts-box").style.filter = "blur(3px)";
+  }
+
+  //handles search functionality using the firebase
+  //edit this later depending on what's in the firebase and how things are organized
+  function handleSearchPosts(keyword) {
+    var postsRef = firebase.database().ref("posts");
+
+    postsRef
+      .orderByChild("title")
+      .equalTo(keyword)
+      .once("value")
+      .then(function (snapshot) {
+        // Handle search results
+        snapshot.forEach(function (childSnapshot) {
+          var postData = childSnapshot.val();
+          console.log("Title: " + postData.title);
+          console.log("Content: " + postData.content);
+        });
+      })
+      .catch(function (error) {
+        console.error("Error searching posts: " + error);
+      });
   }
 
   //adds listeners to the buttons
@@ -44,13 +74,18 @@ document.addEventListener("DOMContentLoaded", function () {
     handleDraftClick(draftButton);
   });
 
-  //accesses firebase for the given email and password
+  var searchButton = document.getElementById("search-btn");
+  searchButton.addEventListener("click", function (event) {
+    handleSearchPosts(document.getElementById("search-txt").value);
+  });
 
-  // firebase
-  //   .auth()
-  //   .signInWithEmailAndPassword(email, password)
-  //   .then((userCredential) => {
-  //     // User is signed in, you can now access the database
-  //     var user = userCredential.user;
-  //   });
+  //accesses firebase for the given email and password
+  // for the posts
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // User is signed in, you can now access the database
+      var user = userCredential.user;
+    });
 });
